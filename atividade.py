ed8bc3a34f2c9a96405fd0656a7aa54cebd966ba
Gustavo <gustavobb@al.insper.edu.br>
2020-05-08 06:39:01 -0300

    grafico

--- atividade.py
+++ atividade.py
@@ -1,10 +1,13 @@
 import math
+import matplotlib.pyplot as pyplot
 
 l = 0.3
 h = 15
 p = 0.03*4
 a = 0.03**2
 k = 200
+t_inf = 300
+tb = 350
 m = math.sqrt((h*p)/(k*a))
 
 temp_kelvin = []
@@ -17,11 +20,7 @@ for i in range(divisions):
 
 for x in x_list:
     t = (math.cosh(m*(l - x)) + (h/(m*k)) * math.sinh(m*(l-x)))/(math.cosh(m*l) + (h/(m*k)) * math.sinh(m*l))
-    temp_kelvin.append(t)
+    temp_kelvin.append(t*(tb - t_inf) + t_inf)
 
-t_inf = 300
-tb = 350
-
-theta_b = tb - t_inf
-T = temp_kelvin[30]*theta_b + t_inf
-print(T)
+pyplot.plot(x_list, temp_kelvin)
+pyplot.show()
